09ab49c843de5eb6fd0ae86cd8725983e49188fa
Kyle Jennings <kyle.jennings@carbonblack.com>
2014-03-06 19:43:13 -0600

    Adding back some removed tests

--- client_apis/python/src/tests/intergration/test_cbapi.py
+++ client_apis/python/src/tests/intergration/test_cbapi.py
@@ -21,29 +21,37 @@ from cbapi.cbapi import CbApi
 cb = None
 
 class CbApiTestCase(unittest.TestCase):
-    def test_info(self):
-        cb.info()
+     def test_info(self):
+         cb.info()
 
-    def test_sensors_plain(self):
-        cb.sensors()
+     def test_license(self):
+         cb.license_status()
 
-    def test_sensors_ip_query(self):
-        cb.sensors({'ip':'255.255.255.255'})
+     def test_sensors_plain(self):
+         cb.sensors()
 
-    def test_sensors_hostname_query(self):
-        cb.sensors({'hostname':'unlikely_host_name'})
+     def test_sensors_ip_query(self):
+         cb.sensors({'ip':'255.255.255.255'})
 
-    def test_binary_stuff(self):
-        binaries = cb.binary_search("")
-        for binary in binaries['results']:
-            cb.binary_summary(binary['md5'])
-            cb.binary(binary['md5'])
+     def test_sensors_hostname_query(self):
+         cb.sensors({'hostname':'unlikely_host_name'})
 
-    def test_process_stuff(self):
-        processes = cb.process_search("")
-        for process in processes['results']:
-            process_summary = cb.process_summary(process['id'], process['segment_id'])
-            process_events = cb.process_events(process['id'], process['segment_id'])
+     def test_binary_stuff(self):
+         binaries = cb.binary_search("")
+         for binary in binaries['results']:
+             cb.binary_summary(binary['md5'])
+             cb.binary(binary['md5'])
+
+     def test_process_stuff(self):
+         processes = cb.process_search("")
+         for process in processes['results']:
+             process_summary = cb.process_summary(process['id'], process['segment_id'])
+             process_events = cb.process_events(process['id'], process['segment_id'])
+
+     def test_watchlist_stuff(self):
+         watchlists = cb.watchlist()
+         for watchlist in watchlists:
+             cb.watchlist(watchlist['id'])
 
 if __name__ == '__main__':
     if 3 != len(sys.argv):
