802169194d85013401dfc305bbba67bdc681a726
Scott Lundgren <scott.lundgrne@carbonblack.com>
2014-05-02 07:17:26 -0700

    - minor additional commenting

--- server_apis/python/example/subscribe_all.py
+++ server_apis/python/example/subscribe_all.py
@@ -1,8 +1,9 @@
 import pika
+import pprint
 
 def on_message(channel, method_frame, header_frame, body):
-    print method_frame.delivery_tag
-    print body
+
+    pprint.pprint(body)
     print
     channel.basic_ack(delivery_tag=method_frame.delivery_tag)
 
@@ -11,15 +12,20 @@ if __name__ == "__main__":
     
     # Set the connection parameters to connect to rabbit-server1 on port 5672
     # on the / virtual host using the username "guest" and password "guest"
-    credentials = pika.PlainCredentials('guest', 'guest')
-    parameters = pika.ConnectionParameters('rabbit-server1',
-                                           5672,
+    credentials = pika.PlainCredentials('cb', '97oQ7kQTdByddQjy')
+    parameters = pika.ConnectionParameters('localhost',
+                                           5004,
                                            '/',
                                            credentials)
 
     connection = pika.BlockingConnection(parameters)
     channel = connection.channel()
-    channel.basic_consume(on_message, 'test')
+
+    channel.queue_declare(queue='dennis')
+
+    channel.queue_bind(exchange='api.events', queue='dennis', routing_key='#')
+
+    channel.basic_consume(on_message, queue='dennis')
     try:
         channel.start_consuming()
     except KeyboardInterrupt:
