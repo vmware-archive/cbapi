9f4c303e91fcc238dac014a659ffc0a1ba7becce
scottlundgren <scott.lundgren@carbonblack.com>
2014-10-11 13:17:46 -0500

    minor update to inline docs in cbapi

--- client_apis/python/src/cbapi/cbapi.py
+++ client_apis/python/src/cbapi/cbapi.py
@@ -15,7 +15,7 @@ class CbApi(object):
     import cbapi
     cb = cbapi.CbApi("http://cb.example.com", token="apitoken")
     # get metadata for all svchost.exe's not from c:\\windows
-    procs = cb.processes(r"process_name:svchost.exe -path:c:\\windows\\")  
+    procs = cb.process_search(r"process_name:svchost.exe -path:c:\\windows\\")  
     for proc in procs['results']:
         proc_detail = cb.process(proc['id'])
         print proc_detail['process']['start'], proc_detail['process']['hostname'], proc_detail['process']['path']
