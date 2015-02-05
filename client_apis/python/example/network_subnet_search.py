797418fb72ccc184210a0b1a4fadb0a1e24e418f
Scott Lundgren <scott.lundgrne@carbonblack.com>
2015-02-04 16:28:19 -0800

    - add option to output some basic stats (--stats) at end of run - bit of refactoring to allow for stat calculation - some hardening against processing errors

--- client_apis/python/example/network_subnet_search.py
+++ client_apis/python/example/network_subnet_search.py
@@ -3,7 +3,6 @@ import struct
 import socket
 from optparse import OptionParser
 
-
 # in the github repo, cbapi is not in the example directory
 sys.path.append('../src/cbapi')
 
@@ -14,41 +13,92 @@ class CBQuery(object):
         self.cb = CbApi(url, token=token, ssl_verify=ssl_verify)
         self.cb_url = url
 
+        # set up some stat tracking
+        self.stats = {}
+        self.stats['total_processes'] = 0
+        self.stats['total_netconns'] = 0
+        self.stats['matching_netconns'] = 0
+        self.stats['output_errors'] = 0
+        self.stats['process_errors'] = 0
+
+    def getStats(self):
+        """
+        return the current statistics
+        """
+        return self.stats
+
+    def outputNetConn(self, proc, netconn):
+        """
+        output a single netconn event from a process document
+        the caller is responsible for ensuring that the document
+        meets start time and subnet criteria
+        """
+
+        # for convenience, use locals for some process metadata fields
+        hostname = proc.get("hostname", "<unknown>")
+        process_name = proc.get("process_name", "<unknown>")
+        user_name = proc.get("username", "<unknown>")
+        process_md5 = proc.get("process_md5", "<unknown>")
+        cmdline = proc.get("cmdline", "<unknown>")
+        path = proc.get("path", "<unknown>")
+        procstarttime = proc.get("start", "<unknown>")
+        proclastupdate = proc.get("last_update", "<unknown>")
+
+        # split the netconn into component parts
+        ts, ip, port, proto, domain, dir = netconn.split("|")
+
+        # get the dotted-quad string representation of the ip
+        str_ip = socket.inet_ntoa(struct.pack("!i", int(ip)))
+                    
+        # the underlying data model provides the protocol number
+        # convert this to human-readable strings (tcp or udp)
+        if "6" == proto:
+            proto = "tcp"
+        elif "17" == proto:
+            proto = "udp"
+                  
+        # the underlying data model provides a boolean indication as to
+        # if this is an inbound or outbound network connection 
+        if "true" == dir:
+            dir = "out"
+        else:
+           dir = "in" 
+
+        # print the record, using pipes as a delimiter
+        print "%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|" % (procstarttime,proclastupdate,hostname, user_name, proto, str_ip, port, dir, domain, process_name, process_md5, path, cmdline)
+
+    def addressInNetwork(self, ip, cidr):
+
+        # the ip can be the emtpy string ('') in cases where the connection
+        # is made via a web proxy.  in these cases the sensor cannot report
+        # the true remote IP as DNS resolution happens on the web proxy (and
+        # not the endpoint)
+        if '' == ip:
+            return False
+
+        try:
+            net = cidr.split('/')[0]
+            bits = cidr.split('/')[1]
+
+            if int(ip) > 0: 
+                ipaddr = struct.unpack('<L', socket.inet_aton(ip))[0]
+            else:
+                ipaddr = struct.unpack('<L', socket.inet_aton(".".join(map(lambda n: str(int(ip)>>n & 0xFF), [24,16,8,0]))))[0]
+            netaddr = struct.unpack('<L', socket.inet_aton(net))[0]
+            netmask = ((1L << int(bits)) - 1)
+                
+            return ipaddr & netmask == netaddr & netmask
+
+        except:
+            return False
+
     def report(self, result, subnet):
 
-        def addressInNetwork(ip, cidr):
-
-            try:
-                net = cidr.split('/')[0]
-                bits = cidr.split('/')[1]
-
-                if int(ip) > 0: 
-                    ipaddr = struct.unpack('<L', socket.inet_aton(ip))[0]
-                else:
-                    ipaddr = struct.unpack('<L', socket.inet_aton(".".join(map(lambda n: str(int(ip)>>n & 0xFF), [24,16,8,0]))))[0]
-                netaddr = struct.unpack('<L', socket.inet_aton(net))[0]
-                netmask = ((1L << int(bits)) - 1)
-                
-                return ipaddr & netmask == netaddr & netmask
-
-            except:
-                return False
-    
         # return the events associated with this process segment
         # this will include netconns, as well as modloads, filemods, etc.
         events = self.cb.process_events(result["id"], result["segment_id"])
         
         proc = events["process"]
-
-        # for convenience, use locals for some process metadata fields
-        hostname = result.get("hostname", "<unknown>")
-        process_name = result.get("process_name", "<unknown>")
-        user_name = result.get("username", "<unknown>")
-        process_md5 = result.get("process_md5", "<unknown>")
-        cmdline = result.get("cmdline", "<unknown>")
-        path = result.get("path", "<unknown>")
-        procstarttime = result.get("start", "<unknown>")
-        proclastupdate = result.get("last_update", "<unknown>")
 
         # the search criteria (netconn_count:[1 to *]) should ensure that
         # all results have at least one netconn
@@ -56,31 +106,23 @@ class CBQuery(object):
 
             # examine each netconn in turn
             for netconn in proc["netconn_complete"]:
-                
+        
+                # update the count of total netconns
+                self.stats['total_netconns'] = self.stats['total_netconns'] + 1
+        
                 # split the netconn event into component parts
                 # note that the port is the remote port in the case of outbound
                 # netconns, and local port in the case of inbound netconns
+                #
+                # for the purpose of this example script, eat any errors
                 ts, ip, port, proto, domain, dir = netconn.split("|")
-                if addressInNetwork(ip, subnet): 
-                    # get the dotted-quad string representation of the ip
-                    str_ip = socket.inet_ntoa(struct.pack("!i", int(ip)))
-                    
-                    # the underlying data model provides the protocol number
-                    # convert this to human-readable strings (tcp or udp)
-                    if "6" == proto:
-                        proto = "tcp"
-                    elif "17" == proto:
-                        proto = "udp"
-                   
-                    # the underlying data model provides a boolean indication as to
-                    # if this is an inbound or outbound network connection 
-                    if "true" == dir:
-                        dir = "out"
-                    else:
-                        dir = "in" 
-
-                    # pring the record, using pipes as a delimiter
-                    print "%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|" % (procstarttime,proclastupdate,hostname, user_name, proto, str_ip, port, dir, domain, process_name, process_md5, path, cmdline)
+                if self.addressInNetwork(ip, subnet): 
+                    try:
+                        self.stats['matching_netconns'] = self.stats['matching_netconns'] + 1
+                        self.outputNetConn(proc, netconn)
+                    except:
+                        self.stats['output_errors'] = self.stats['output_errors'] + 1
+                        raise 
 
     def strip_to_int(ip):
         """
@@ -100,26 +142,34 @@ class CBQuery(object):
             if not end: end = "*"
             if not begin: begin = "*"
             q = "ipaddr:%s %s:[%s TO %s]" % (subnet, datetype, begin, end)
-        print q
 
         # begin with the first result - we'll perform the search in pages 
-        # the default page size is 10 (10 reslts)
+        # the default page size is 10 (10 results)
         start = 0
 
-        # loop over the entire result set
+        # loop over the entire result set, paging as required
         while True:
 
             # get the next page of results 
             procs = self.cb.process_search(q, start=start)
-      
+            
+            # track the total number of matching results
+            self.stats['total_processes'] = procs['total_results']
+
             # if there are no results, we are done paging 
             if len(procs["results"]) == 0:
                 break
 
             # examine each result individually
-            # each result represents a single process segment
+            # each result represents a single segment of a single process
+            #
+            # for the purposes of this example script, eat any errors
+            #
             for result in procs["results"]:
-                self.report(result, subnet)
+                try:
+                    self.report(result, subnet)
+                except Exception, e:
+                    self.stats['process_errors'] = self.stats['process_errors'] + 1
 
             # move forward to the next page 
             start = start + 10
@@ -148,6 +198,8 @@ def build_cli_parser():
                       help="API Token for Carbon Black server")
     parser.add_option("-n", "--no-ssl-verify", action="store_false", default=True, dest="ssl_verify",
                       help="Do not verify server SSL certificate.")
+    parser.add_option("-S", "--stats", action="store_true", default=False, dest='stats',
+                      help="Output stats at end of run.")
     parser.add_option("-s", "--subnet", action="store", default=None, dest="subnet",
                       help="Subnet, as specified in CIDR notation e.g. 127.0.0.1/32, to query for network traffic")
     parser.add_option("-b", "--begin", action="store", default=None, dest="begin",
@@ -178,5 +230,16 @@ def main(argv):
 
     cb.check(opts.subnet, opts.datetype, opts.begin, opts.end)
 
+    stats = cb.getStats()
+
+    if opts.stats:
+        print
+        print "%-30s | %s" % ("Total Matching Processes", stats['total_processes'])
+        print "%-30s | %s" % ("Total NetConns", stats['total_netconns'])
+        print "%-30s | %s" % ("Matching NetConns", stats['matching_netconns'])
+        print
+        print "%-30s | %s" % ("Processing Errors", stats['process_errors'])
+        print "%-30s | %s" % ("Output Errors", stats['output_errors'])
+ 
 if __name__ == "__main__":
     sys.exit(main(sys.argv[1:]))
