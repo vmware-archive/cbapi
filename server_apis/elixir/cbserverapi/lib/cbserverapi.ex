bb4dc6bdc6fcd7e223f755e08a32b70c4f8a6295
Red Davies <red@infect.me>
2015-03-23 18:29:39 +0000

    Trimmed logic external to the defmacro

--- server_apis/elixir/cbserverapi/lib/cbserverapi.ex
+++ server_apis/elixir/cbserverapi/lib/cbserverapi.ex
@@ -6,15 +6,13 @@ defmodule Cbserverapi do
     defmacro __using__(_) do
       quote location: :keep do
       require Logger
+
       def start_link(key) do
         GenServer.start_link(__MODULE__, {key, []}, name: String.to_atom("CB_" <> key))
       end
 
-      def init({key, _state}) do
-        {:ok, [channel, queue]} = connect(key)
-        sub = basic_consume(queue: queue, no_ack: true)
-        basic_consume_ok(consumer_tag: consumer_tag) = :amqp_channel.subscribe channel, sub, self
-        {:ok, [consumer_tag, channel, queue]}
+      def init(incoming) do
+        Cbserverapi.init(incoming)
       end
     
       def handle_info({:"basic.consume_ok", consumer_tag}, state = [consumer_tag,_,_]) do
@@ -24,20 +22,25 @@ defmodule Cbserverapi do
         Logger.info("Unhandled bus message discarded: " <> inspect(stuff))
         { :noreply, state}
       end
-
-      defp connect(key) do
-        channel = getcreds |> Exrabbit.Utils.connect |> Exrabbit.Utils.channel
-        queue = Exrabbit.Utils.declare_queue(channel)
-        {:"queue.bind_ok"} = Exrabbit.Utils.bind_queue(channel, queue, "api.events", key)
-        {:ok, [channel, queue]}
-      end
-
-      defp getcreds do
-        Cbserverapi.Creds.getcreds
-      end
-
-      defoverridable [getcreds: 0]
-
     end
   end
+
+  def init({key, _state}) do
+    {:ok, [channel, queue]} = connect(key)
+     sub = basic_consume(queue: queue, no_ack: true)
+     basic_consume_ok(consumer_tag: consumer_tag) = :amqp_channel.subscribe channel, sub, self
+     {:ok, [consumer_tag, channel, queue]}
+  end
+    
+  defp connect(key) do
+    channel = getcreds |> Exrabbit.Utils.connect |> Exrabbit.Utils.channel
+    queue = Exrabbit.Utils.declare_queue(channel)
+    {:"queue.bind_ok"} = Exrabbit.Utils.bind_queue(channel, queue, "api.events", key)
+    {:ok, [channel, queue]}
+  end
+
+  def getcreds do
+    Cbserverapi.Creds.getcreds
+  end
+
 end
