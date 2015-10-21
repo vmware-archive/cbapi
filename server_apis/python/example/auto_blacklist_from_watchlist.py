8827ab1a032dc3397a49f5d0ae2443053c840c37
Jason McFarland <jmcfarland@bit9.com>
2015-10-21 02:17:25 -0500

    auto blacklist script now takes a config file as a parameter
    The user can specifiy which watchlists will trigger an auto-blacklist.

--- server_apis/python/example/auto_blacklist_from_watchlist.py
+++ server_apis/python/example/auto_blacklist_from_watchlist.py
@@ -1,10 +1,10 @@
-import sys
 import pika
-import pprint
 import random
-import optparse
 import json
 import requests
+import sys
+from ConfigParser import SafeConfigParser
+
 
 def blacklist_binary(md5):
     """
@@ -17,17 +17,17 @@ def blacklist_binary(md5):
 
     headers = {'X-AUTH-TOKEN': cbtoken}
 
-    data = {"md5hash": md5, 
-        "text":"Auto-Blacklist From Watchlist", 
-        "last_ban_time":0, 
-        "ban_count":0,
-        "last_ban_host":0,
-        "enabled":True }
+    data = {"md5hash": md5,
+            "text": "Auto-Blacklist From Watchlist",
+            "last_ban_time": 0,
+            "ban_count": 0,
+            "last_ban_host": 0,
+            "enabled": True}
 
-    r = requests.post("https://%s/api/v1/banning/blacklist" % (cbserver), 
-        headers = headers, 
-        data = json.dumps(data),
-        verify = False)
+    r = requests.post("https://%s/api/v1/banning/blacklist" % (cbserver),
+                      headers=headers,
+                      data=json.dumps(data),
+                      verify=False)
 
     if r.status_code == 409:
         print "This md5 hash is already blacklisted"
@@ -47,79 +47,142 @@ def on_message(channel, method_frame, header_frame, body):
 
             if method_frame.routing_key == 'watchlist.hit.binary':
                 print "watchlist.hit.binary consume"
-                
+
                 parsed_json = json.loads(body)
-                print parsed_json
-                lst = parsed_json['docs']
-                for item in lst:
-                    blacklist_binary(item['md5'])
-            
+                if parsed_json['watchlist_id'] in watchlistsdict.values():
+                    lst = parsed_json['docs']
+                    for item in lst:
+                        blacklist_binary(item['md5'])
+
             elif method_frame.routing_key == 'watchlist.hit.process':
                 print "watchlist.hit.process consume"
-                
+
                 parsed_json = json.loads(body)
-                lst = parsed_json['docs']
-                for item in lst:
-                    blacklist_binary(item['process_md5'])
+                if parsed_json['watchlist_id'] in watchlistsdict.values():
+                    lst = parsed_json['docs']
+                    for item in lst:
+                        blacklist_binary(item['process_md5'])
 
     except Exception, e:
         print e
     finally:
-        # need to make sure we ack the messages so they don't get left un-acked in the queue
-        # we set multiple to true to ensure that we ack all previous messages
-        channel.basic_ack(delivery_tag=method_frame.delivery_tag, multiple=True)
-
+        # need to make sure we ack the messages so they don't get left un-acked
+        # in the queue we set multiple to true to ensure that we ack all
+        # previous messages
+        channel.basic_ack(delivery_tag=method_frame.delivery_tag,
+                          multiple=True)
     return
+
 
 def generate_queue_name():
     """
     generates a random queue name
     """
-    return str(random.randint(0,10000)) + "-" + str(random.randint(0,100000))
+    return str(random.randint(0, 10000)) + "-" + str(random.randint(0, 100000))
 
-def build_cli_parser():
 
-    parser = optparse.OptionParser(usage="%prog [options]", 
-        description="Example CBSAPI script to consume published events")
-    
-    parser.add_option("-p", "--password", action="store", default="", dest="password",
-        help="RabbitMQ password; see /etc/cb/cb.conf\nDefault is an empty string")
-    
-    parser.add_option("-u", "--usename", action="store", default="cb", dest="username",
-        help="RabbitMQ username; see /etc/cb/cb.conf\nDefault is cb")
-    
-    parser.add_option("-s", "--cbserverip", action="store", default="localhost", dest="cbserver", 
-        help="Carbon Black server IP for rabbitmq server\nDefault is localhost")
-    
-    parser.add_option("-t", "--cbtoken", action="store", default="", dest="cbtoken",
-        help='Carbon Black API token\nDefault is an empty string')
+def parse_config_file(filename):
+    """
+    Parses the config file passed into this script
+    NOTE: note the conversion to unicode
+    """
 
-    return parser
+    parser = SafeConfigParser()
+    parser.read(filename)
+
+    return (unicode(parser.get("settings", "rabbitmqusername"), "utf-8"),
+            unicode(parser.get("settings", "rabbitmqpassword"), "utf-8"),
+            unicode(parser.get("settings", "cbserverip"), "utf-8"),
+            unicode(parser.get("settings", "cbtoken"), "utf-8"),
+            unicode(parser.get("settings", "watchlists"), "utf-8"))
+
+
+def validate_watchlist_dict(watchlistsdict):
+    """
+    Validates the watchlist dictionary and watchlist names with no
+    associated ID will warn the user that the ID could not be found.
+    This means that auto-blacklisting will not trigger since this script
+    could not find the blacklist on the specified Carbon Black Server
+    """
+
+    for key, value in watchlistsdict.iteritems():
+        if not value:
+            print "Warning: ID for watchlist %s not found." % (key)
+        else:
+            print "Watchlist: %s, ID: %d" % (key, value)
+
+def get_watchlist_id_by_name(watchlistsdict):
+    """
+    For each watchlist name specified in the config file, find the
+    associated watchlist ID.
+    NOTE: We trigger on watchlist IDs, and not on watchlist names
+    """
+    global cbtoken
+    global cbserver
+
+    headers = {'X-AUTH-TOKEN': cbtoken}
+
+    r = requests.get("https://%s/api/v1/watchlist" % (cbserver),
+                      headers=headers,
+                      verify=False)
+
+    parsed_json = json.loads(r.text)
+
+    for watchlist in parsed_json:
+        for key, value in watchlistsdict.iteritems():
+            if watchlist['name'].lower() == key.lower():
+                watchlistsdict[key] = watchlist['id']
+
+def Usage():
+    return ("Usage: python auto_blacklist_from_watchlist.py <config file>")
+
 
 if __name__ == "__main__":
 
-    #    
-    # build the command line parser and ensure the required password option was provided
-    #
-    parser = build_cli_parser()
-    (options, args) = parser.parse_args()
+    if len(sys.argv) != 2:
+        print Usage()
+        exit(0)
 
-    global cbtoken 
-    cbtoken = options.cbtoken
+    configfile = sys.argv[1]
+
+    global cbtoken
+    global watchlistsdict
     global cbserver
-    cbserver = options.cbserver
-    
+
+    #
+    # Parse the config file
+    #
+    (username, password, cbserver, cbtoken, watchlists) = parse_config_file(configfile)
+
+    #
+    # Build a dictionary of "WatchList Name" -> "Watchlist ID"
+    #
+    watchlistsdict = dict.fromkeys([watchlist.strip() for watchlist in watchlists.split(',')])
+
+    #
+    # Populate the values of the dictionary with the watchlist ID.
+    # This function will query the Carbon Black server and find the 
+    # associated ID
+    #
+    get_watchlist_id_by_name(watchlistsdict)
+
+    #
+    # Validate that all the watchlist names have associated IDs.
+    # If a name does not have an associated ID then we just warn the user.
+    #
+    validate_watchlist_dict(watchlistsdict)
+
     #
     # Set the connection parameters to connect to to the rabbitmq:5004
     # using the supplied username and password
     #
-    credentials = pika.PlainCredentials(options.username, 
-        options.password)
-    
+    credentials = pika.PlainCredentials(username,
+                                        password)
+
     #
     # Create our parameters for pika
     #
-    parameters = pika.ConnectionParameters(options.cbserver,
+    parameters = pika.ConnectionParameters(cbserver,
                                            5004,
                                            '/',
                                            credentials)
@@ -144,19 +207,19 @@ if __name__ == "__main__":
     # with events when this program exists.
     channel.queue_declare(queue=queue_name, auto_delete=True)
 
-    channel.queue_bind(exchange='api.events', queue=queue_name, routing_key='#')
+    channel.queue_bind(exchange='api.events', queue=queue_name, routing_key='watchlist.hit.#')
 
     channel.basic_consume(on_message, queue=queue_name)
-   
+
     print
     print "Subscribed to events!"
     print ("Keep this script running to auto-blacklist md5 hashes "
            "from watchlist.hit.process and watchlist.hit.binary hits!")
     print
-    
+
     try:
         channel.start_consuming()
     except KeyboardInterrupt:
         channel.stop_consuming()
-   
+
     connection.close()
